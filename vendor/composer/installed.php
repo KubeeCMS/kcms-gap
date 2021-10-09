aedfb975763c88c94efb0a2eccc498bbc5a49d26
Romeo Covaci <romeo.covaci@gmail.com>
2021-10-09 22:20:56 +0300

    Version: 1.11.1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c7a5a669a02914f2077d169ec21d9c7bb075e377',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c7a5a669a02914f2077d169ec21d9c7bb075e377',
+    ),
+    'skyverge/wc-plugin-framework' => 
+    array (
+      'pretty_version' => '5.10.2',
+      'version' => '5.10.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a8a94ec6cf6d41ab4e23692dd5dcaa4f3876810c',
+    ),
+  ),
+);
